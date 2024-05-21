ccb7ba91eb3b2dd8960299f13b60e3cd4b726634
wegar2 <awegrzyn17@gmail.com>
2024-05-22 00:09:29 +0200

    getting function name from within the function

---
+++ get_function_name_from_within_the_function.py
@@ -0,0 +1,22 @@
+import inspect
+from inspect import stack
+
+
+def my_add(x: int, y: int) -> int:
+    print(f"{stack()[0][3]=}")
+    print(f"{inspect.currentframe().f_code.co_name=}")
+    print(f"{get_current_function_name()=}")
+    return x + y
+
+
+def get_current_function_name() -> str:
+    return stack()[1][3]
+
+
+if __name__ == "__main__":
+    my_add(12, 1)
+
+    # Running this prints:
+    # stack()[0][3]='my_add'
+    # inspect.currentframe().f_code.co_name='my_add'
+    # get_current_function_name()='my_add'
