f7d111d14cf21b63bec9533f882ed2c979eb48c3
wegar2 <awegrzyn17@gmail.com>
2024-06-08 00:26:57 +0200

    padding numbers with zeros in fstrigns

--- fstrings/fstrings01_numbers.py
+++ fstrings/fstrings01_numbers.py
@@ -20,9 +20,11 @@ if __name__ == "__main__":
 
     # 4. displaying a number float with comma as thousands separator and dot as
     #    decimal separator
-    x = 12345.678
+    x = 12345.67811111
     print(f"x = {x:0,.4f}")
     # NOTE!: another thousands separator that can be used is underscore
     print(f"x = {x:_}")
     # REMARK: there are only two thousands separators that can be used with
     # f-strings: underscore and comma
+    # print(f"x = {x:_9.2f}")
+    print(f"x = {x:9_.2f}")
