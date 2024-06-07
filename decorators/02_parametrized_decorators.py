7d21fa6a8aca31bdb39661aecb3060a86466a5b8
wegar2 <awegrzyn17@gmail.com>
2024-06-07 22:20:35 +0200

    an edit of 02_...

--- decorators/02_parametrized_decorators.py
+++ decorators/02_parametrized_decorators.py
@@ -15,7 +15,10 @@ def conditional_sum(x, y):
 
 
 def parametrized_decorator(multiplier: int = 2) -> Callable:
+    print(f"{multiplier=}")
+
     def outer_wrapper(func: Callable):
+        print(f"{func=}")
         @wraps(func)
         def inner_wrapper(*args, **kwargs):
             logger.info(f"Running function {func.__name__} with {args=} and "
@@ -26,10 +29,16 @@ def parametrized_decorator(multiplier: int = 2) -> Callable:
                 d[k] = multiplier * d[k] if v is not None else d[k]
             return d
         return inner_wrapper
+    print(f"{outer_wrapper=}")
     return outer_wrapper
 
 
-@parametrized_decorator(multiplier=2)
+# NOTE: Compare runs for both cases below in order to understand what the
+# difference is:
+# (a) decorator gets an argument
+# (b) decorator is left without argument
+# @parametrized_decorator # won't run properly
+@parametrized_decorator(multiplier=3)
 def custom_sum(*args, **kwargs) -> dict:
     logger.info("calculating sums of args and kwargs")
     if args is not None:
@@ -53,4 +62,17 @@ def custom_sum(*args, **kwargs) -> dict:
 
 
 if __name__ == "__main__":
+
     print(custom_sum(1, 32, "a", {123}, a=1, b=2))
+
+    # Explanation:
+    # (a) if the decorator is left with argument, the execution proceeds by
+    # passing multiplier = 3 and the decorator function returns its
+    # outer_function
+    # (b) However, if decorator is left without argument it takes the function
+    # as its input, i.e. it assigns it to multiplier.
+    # It does not take a big leap of faith to imagine why the function
+    # execution fails in this scenario
+    # Thus, the solutions given in this script are of decorators with
+    # MANDATORY arguments. Check the next script for examples
+    # of how to write decorators with optional arguments.
