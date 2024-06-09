292874ec6f8c7e7f959bd1264e6f5725b31220d3
wegar2 <awegrzyn17@gmail.com>
2024-06-09 20:40:12 +0200

    example of defining managed property by passing callables to property() added; started rearranging metaclasses examples (unfinished)

---
+++ pyoop/oop01_create_property_from_methods.py
@@ -0,0 +1,20 @@
+from typing import TypeAlias, Union
+
+Numeric: TypeAlias = Union[float, int]
+
+
+class Point1d:
+
+    def __init__(self, x: Numeric):
+        self._x: float = float(x)
+
+    def get_x(self) -> float:
+        return self._x
+
+    def set_x(self, x):
+        self._x = float(x)
+
+    x = property(
+        fget=get_x,  # noqa
+        fset=set_x,  # noqa
+    )
