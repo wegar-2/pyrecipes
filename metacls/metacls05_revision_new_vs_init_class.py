a843d6f19002b2095f94250ce30bd64af0af37ef
wegar2 <awegrzyn17@gmail.com>
2024-06-15 22:31:04 +0200

    started revision of metaclass

---
+++ metacls/metacls05_revision_new_vs_init_class.py
@@ -0,0 +1,23 @@
+
+
+class A:
+    """
+    Using class A to review how the magic methods: __new__ and __init__ work.
+    """
+
+    def __new__(cls, x, y):
+        print(f"Inside __new__; {x=}, {y=}")
+        object_ = super().__new__(cls)
+        object_.x = x
+        return object_
+
+    def __init__(self, x, y):
+        print(f"Inside __init__; {x=}, {y=}")
+        self.y = y
+
+
+if __name__ == "__main__":
+    a = A.__new__(A, 23, 33)
+    print(f"before init: {a.__dict__=}")
+    A.__init__(a, 23, 33)
+    print(f"after init: {a.__dict__=}")
