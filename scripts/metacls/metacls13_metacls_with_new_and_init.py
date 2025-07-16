b1ea8f5816435951dd138662b501b1ceb3066064
wegar2 <awegrzyn17@gmail.com>
2025-07-16 23:59:00 +0200

    setting accesor and getter in __new__

--- scripts/metacls/metacls13_metacls_with_new_and_init.py
+++ scripts/metacls/metacls13_metacls_with_new_and_init.py
@@ -5,6 +5,17 @@ class MetaClass(type):
     def __new__(mcls, name, bases, class_dict, **kwargs):
         print("Inside __new__")
         new_class = type.__new__(mcls, name, bases, class_dict)
+
+        def set_value(self, value):
+            self.value = value
+
+        def get_value(self):
+            return self.value
+
+        new_class.prop = property(
+            fset=set_value,
+            fget=get_value
+        )
         return new_class
 
     def __init__(self, name, bases, class_dict, **kwargs):
@@ -18,3 +29,6 @@ class Klass(metaclass=MetaClass):
 
 if __name__ == "__main__":
     obj = Klass()
+
+    obj.value = 123
+    print(f"{obj.value=}")
