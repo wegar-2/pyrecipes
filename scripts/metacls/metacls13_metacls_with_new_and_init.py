43f62bd75df5186d5277a0cfe595e8f0cbc01bc8
wegar2 <awegrzyn17@gmail.com>
2025-07-17 00:16:18 +0200

    extended example to do some silly things -- i.e. setting class variables in init of a metaclass is theoretically the right thing to do

--- scripts/metacls/metacls13_metacls_with_new_and_init.py
+++ scripts/metacls/metacls13_metacls_with_new_and_init.py
@@ -1,9 +1,11 @@
+import random
 
 
 class MetaClass(type):
 
     def __new__(mcls, name, bases, class_dict, **kwargs):
-        print("Inside __new__")
+        print(f"Inside {mcls.__name__} method __new__; "
+              f"{mcls=}; {name=}; {bases=}; {class_dict=}; {kwargs=}")
         new_class = type.__new__(mcls, name, bases, class_dict)
 
         def set_value(self, value):
@@ -12,23 +14,52 @@ class MetaClass(type):
         def get_value(self):
             return self.value
 
+        def get_instance_count(cls):
+            return cls.instance_count
+
+        def increment_instance_count(cls):
+            cls.instance_count = cls.instance_count + 1
+
         new_class.prop = property(
             fset=set_value,
             fget=get_value
         )
+        new_class.get_instance_count = classmethod(get_instance_count)
+        new_class.increment_instance_count = (
+            classmethod(increment_instance_count))
         return new_class
 
-    def __init__(self, name, bases, class_dict, **kwargs):
-        print("Inside __init__")
-        super().__init__(self)
+    def __init__(
+            cls,
+            name,
+            bases,
+            class_dict,
+            **kwargs
+    ):
+        print(f"Inside {cls.__class__.__name__} method __init__; "
+              f"{cls=}; {name=}; {bases=}; {class_dict=}; {kwargs=}")
+        super().__init__(cls)
+        print(f"Setting class {cls.__name__} counter to zero")
+        cls.instance_count = 0
 
 
 class Klass(metaclass=MetaClass):
-    pass
+    def __init__(self):
+        random_value = random.uniform(0, 1)
+        print(f"Inside class {self.__class__.__name__} __init__; "
+              f"setting the value to {random_value}")
+        self.value = random_value
+        instance_num: int = getattr(self, "get_instance_count")() + 1
+        getattr(self.__class__, "increment_instance_count")()
+        print(f"Completed creation of instance #{instance_num:05}")
 
 
 if __name__ == "__main__":
-    obj = Klass()
 
+    obj = Klass()
     obj.value = 123
     print(f"{obj.value=}")
+
+    more_instances: list[Klass] = []
+    for _ in range(10):
+        more_instances.append(Klass())
